5f20061886c8e4bc7432cc27c55f5f9067e72699
Raja Singh Samuel <rajasinghsamuelb@karunya.edu.in>
2020-08-15 15:06:15 +0530

    fixed ui hobby ... pending

--- app/src/main/java/com/urk17cs290/studentinformationapp/DisplayOutput.java
+++ app/src/main/java/com/urk17cs290/studentinformationapp/DisplayOutput.java
@@ -1,17 +1,106 @@
 package com.urk17cs290.studentinformationapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textview.MaterialTextView;
 
 public class DisplayOutput extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_output);
-        Intent g=getIntent();
-        g.getExtras();
+
+        MaterialTextView name = findViewById(R.id.name);
+        MaterialTextView dname = findViewById(R.id.deptName);
+        MaterialTextView regno = findViewById(R.id.regNo);
+        MaterialTextView gend = findViewById(R.id.gender);
+        MaterialTextView State = findViewById(R.id.state);
+        MaterialTextView subj1 = findViewById(R.id.subject1);
+        MaterialTextView subj2 = findViewById(R.id.subject2);
+        MaterialTextView subj3 = findViewById(R.id.subject3);
+        MaterialTextView subj4 = findViewById(R.id.subject4);
+        MaterialTextView subj5 = findViewById(R.id.subject5);
+        MaterialTextView mk1 = findViewById(R.id.mark1);
+        MaterialTextView mk2 = findViewById(R.id.mark2);
+        MaterialTextView mk3 = findViewById(R.id.mark3);
+        MaterialTextView mk4 = findViewById(R.id.mark4);
+        MaterialTextView mk5 = findViewById(R.id.mark5);
+        MaterialTextView Cgpa = findViewById(R.id.cgpa);
+        MaterialTextView gd1 = findViewById(R.id.grade1);
+        MaterialTextView gd2 = findViewById(R.id.grade2);
+        MaterialTextView gd3 = findViewById(R.id.grade3);
+        MaterialTextView gd4 = findViewById(R.id.grade4);
+        MaterialTextView gd5 = findViewById(R.id.grade5);
+        MaterialTextView Hob = findViewById(R.id.hobby);
+
+
+        Intent i = getIntent();
+        String Name = i.getStringExtra("NAME");
+        String regNo = i.getStringExtra("REG_NO");
+        String dept = i.getStringExtra("DEPT_NAME");
+        String gender = i.getStringExtra("GENDER");
+        String state = i.getStringExtra("STATE");
+        String sub1 = i.getStringExtra("SUB_1");
+        String sub2 = i.getStringExtra("SUB_2");
+        String sub3 = i.getStringExtra("SUB_3");
+        String sub4 = i.getStringExtra("SUB_4");
+        String sub5 = i.getStringExtra("SUB_5");
+        String mark1 = i.getStringExtra("MARK_1");
+        String mark2 = i.getStringExtra("MARK_2");
+        String mark3 = i.getStringExtra("MARK_3");
+        String mark4 = i.getStringExtra("MARK_4");
+        String mark5 = i.getStringExtra("MARK_5");
+        String cgpa = i.getStringExtra("CGPA");
+        String grade1 = i.getStringExtra("GRADE1");
+        String grade2 = i.getStringExtra("GRADE2");
+        String grade3 = i.getStringExtra("GRADE3");
+        String grade4 = i.getStringExtra("GRADE4");
+        String grade5 = i.getStringExtra("GRADE5");
+        String fCheck = i.getStringExtra("F_CHECK");
+        String vCheck = i.getStringExtra("V_CHECK");
+        String bCheck = i.getStringExtra("B_CHECK");
+        String gCheck = i.getStringExtra("G_CHECK");
+
+
+        name.setText(Name);
+        dname.setText(dept);
+        regno.setText(regNo);
+        gend.setText(gender);
+        State.setText(state);
+        subj1.setText(sub1);
+        subj2.setText(sub2);
+        subj3.setText(sub3);
+        subj4.setText(sub4);
+        subj5.setText(sub5);
+        mk1.setText(mark1);
+        mk2.setText(mark2);
+        mk3.setText(mark3);
+        mk4.setText(mark4);
+        mk5.setText(mark5);
+        Cgpa.setText(cgpa);
+        gd1.setText(grade1);
+        gd2.setText(grade2);
+        gd3.setText(grade3);
+        gd4.setText(grade4);
+        gd5.setText(grade5);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        if (vCheck == "true")
+            stringBuilder.append("Video Games,");
+        if (fCheck == "true")
+            stringBuilder.append("Foot Ball,");
+        if (bCheck == "true")
+            stringBuilder.append("Board Games,");
+        if (gCheck == "true")
+            stringBuilder.append("Gardening");
+        String hobby = stringBuilder.toString();
+        Log.e("TAG", "onCreate: hobby:" + hobby);
+        Hob.setText(hobby);
     }
 }
--- app/src/main/java/com/urk17cs290/studentinformationapp/MainActivity.java
+++ app/src/main/java/com/urk17cs290/studentinformationapp/MainActivity.java
@@ -1,127 +1,125 @@
 package com.urk17cs290.studentinformationapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
-import com.google.android.material.textfield.TextInputEditText;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.textfield.TextInputLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String FALSE = "false";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        final TextInputLayout name= findViewById(R.id.name);
-//        final TextInputLayout regno= findViewById(R.id.regNo);
-//        final TextInputLayout dept= findViewById(R.id.deptName);
-//        final RadioGroup Gender= findViewById(R.id.gender);
-//        final RadioButton Male= findViewById(R.id.male);
-//        final RadioButton FeMale= findViewById(R.id.female);
-//        final Spinner States=findViewById(R.id.state);
-//        Button Submit=findViewById(R.id.submitButton);
-//        final  TextInputLayout sub1=findViewById(R.id.subject1);
-//        final  TextInputLayout sub2=findViewById(R.id.subject2);
-//        final  TextInputLayout sub3=findViewById(R.id.subject3);
-//        final  TextInputLayout sub4=findViewById(R.id.subject4);
-//        final  TextInputLayout sub5=findViewById(R.id.subject5);
-//        final  TextInputLayout mark1=findViewById(R.id.mark1);
-//        final  TextInputLayout mark2=findViewById(R.id.mark2);
-//        final  TextInputLayout mark3=findViewById(R.id.mark3);
-//        final  TextInputLayout mark4=findViewById(R.id.mark4);
-//        final  TextInputLayout mark5=findViewById(R.id.mark5);
-//
-//        final CheckBox football=findViewById(R.id.football);
-//        final CheckBox gardening=findViewById(R.id.gardening);
-//        final CheckBox boardGames=findViewById(R.id.boardGames);
-//        final CheckBox videoGames=findViewById(R.id.videoGames);
-//
-//
+        final TextInputLayout name = findViewById(R.id.name);
+        final TextInputLayout regno = findViewById(R.id.regNo);
+        final TextInputLayout dept = findViewById(R.id.deptName);
+        final RadioGroup gender = findViewById(R.id.gender);
+        final Spinner states = findViewById(R.id.state);
+        Button submit = findViewById(R.id.submitButton);
+        final TextInputLayout sub1 = findViewById(R.id.subject1);
+        final TextInputLayout sub2 = findViewById(R.id.subject2);
+        final TextInputLayout sub3 = findViewById(R.id.subject3);
+        final TextInputLayout sub4 = findViewById(R.id.subject4);
+        final TextInputLayout sub5 = findViewById(R.id.subject5);
+        final TextInputLayout mark1 = findViewById(R.id.mark1);
+        final TextInputLayout mark2 = findViewById(R.id.mark2);
+        final TextInputLayout mark3 = findViewById(R.id.mark3);
+        final TextInputLayout mark4 = findViewById(R.id.mark4);
+        final TextInputLayout mark5 = findViewById(R.id.mark5);
+
+        final CheckBox football = findViewById(R.id.football);
+        final CheckBox gardening = findViewById(R.id.gardening);
+        final CheckBox boardGames = findViewById(R.id.boardGames);
+        final CheckBox videoGames = findViewById(R.id.videoGames);
+
+        submit.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        try {
+                            final String Name = Objects.requireNonNull(name.getEditText()).getText().toString();
+                            final String regNo = Objects.requireNonNull(regno.getEditText()).getText().toString();
+                            final String deptName = Objects.requireNonNull(dept.getEditText()).getText().toString();
+                            RadioButton selectedGender = findViewById(gender.getCheckedRadioButtonId());
+                            final String gend = selectedGender.getText().toString().trim();
+                            String state = states.getSelectedItem().toString().trim();
+
+                            double m1 = Double.parseDouble(Objects.requireNonNull(mark1.getEditText()).getText().toString().trim());
+                            double m2 = Double.parseDouble(Objects.requireNonNull(mark2.getEditText()).getText().toString().trim());
+                            double m3 = Double.parseDouble(Objects.requireNonNull(mark3.getEditText()).getText().toString().trim());
+                            double m4 = Double.parseDouble(Objects.requireNonNull(mark4.getEditText()).getText().toString().trim());
+                            double m5 = Double.parseDouble(Objects.requireNonNull(mark5.getEditText()).getText().toString().trim());
+                            double tot = m1 + m2 + m3 + m4 + m5;
+                            double avg = tot / 5;
+                            double cgpa = avg / 10;
+                            String fCheck = FALSE;
+                            String gCheck = FALSE;
+                            String bCheck = FALSE;
+                            String vCheck = FALSE;
+
+                            if (football.isChecked()) fCheck = "true";
+                            if (gardening.isChecked()) gCheck = "true";
+                            if (boardGames.isChecked()) bCheck = "true";
+                            if (videoGames.isChecked()) vCheck = "true";
+                            String[] grades = new String[5];
+                            double[] marks = {m1, m2, m3, m4, m5};
+                            for (int i = 0; i < marks.length; i++) {
+                                if (marks[i] > 95) grades[i] = "O";
+                                else if (marks[i] > 85) grades[i] = "S";
+                                else if (marks[i] > 75) grades[i] = "A";
+                                else if (marks[i] > 65) grades[i] = "B";
+                                else if (marks[i] > 50) grades[i] = "P";
+                                else grades[i] = "F";
+                            }
+
+                            Intent i = new Intent(getApplicationContext(), DisplayOutput.class);
+                            i.putExtra("NAME", Name);
+                            i.putExtra("REG_NO", regNo);
+                            i.putExtra("DEPT_NAME", deptName);
+                            i.putExtra("GENDER", gend);
+                            i.putExtra("STATE", state);
+                            i.putExtra("SUB_1", Objects.requireNonNull(sub1.getEditText()).getText().toString().trim());
+                            i.putExtra("SUB_2", Objects.requireNonNull(sub2.getEditText()).getText().toString());
+                            i.putExtra("SUB_3", Objects.requireNonNull(sub3.getEditText()).getText().toString());
+                            i.putExtra("SUB_4", Objects.requireNonNull(sub4.getEditText()).getText().toString());
+                            i.putExtra("SUB_5", Objects.requireNonNull(sub5.getEditText()).getText().toString());
+                            i.putExtra("MARK_1", mark1.getEditText().getText().toString());
+                            i.putExtra("MARK_2", mark2.getEditText().getText().toString());
+                            i.putExtra("MARK_3", mark3.getEditText().getText().toString());
+                            i.putExtra("MARK_4", mark4.getEditText().getText().toString());
+                            i.putExtra("MARK_5", mark5.getEditText().getText().toString());
+                            i.putExtra("CGPA", Double.toString(cgpa));
+                            i.putExtra("GRADE1", grades[0]);
+                            i.putExtra("GRADE2", grades[1]);
+                            i.putExtra("GRADE3", grades[2]);
+                            i.putExtra("GRADE4", grades[3]);
+                            i.putExtra("GRADE5", grades[4]);
+                            i.putExtra("F_CHECK", fCheck);
+                            i.putExtra("B_CHECK", bCheck);
+                            i.putExtra("V_CHECK", vCheck);
+                            i.putExtra("G_CHECK", gCheck);
+                            startActivity(i);
 
 
-//        Submit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//
-//                final String Name=name.getText().toString();
-//                final String regNo =regno.getText().toString();
-//                final String deptName =dept.getText().toString();
-//                RadioButton selectedGender=(RadioButton)findViewById(Gender.getCheckedRadioButtonId());
-//                final String gend=selectedGender.getText().toString();
-//                String state = States.getSelectedItem().toString();
-//
-//                double total=Double.parseDouble(mark1.get().toString())+
-//                        Double.parseDouble(mark2.getText().toString())+
-//                        Double.parseDouble(mark3.getText().toString())+
-//                        Double.parseDouble(mark4.getText().toString())+
-//                        Double.parseDouble(mark5.getText().toString());
-//                double cgpa=total/5;
-//
-//                boolean fCheck = false,gCheck=false,bCheck=false,vCheck=false;
-//                if(football.isChecked())
-//                    fCheck=true;
-//                if(gardening.isChecked())
-//                    gCheck=true;
-//                if(boardGames.isChecked())
-//                    bCheck=true;
-//                if(videoGames.isChecked())
-//                    vCheck=true;
-//                String grade;
-//
-//                if (cgpa>95)
-//                    grade="O";
-//                else if(cgpa>85)
-//                    grade="S";
-//                else if(cgpa>75)
-//                    grade="A";
-//                else if(cgpa>65)
-//                    grade="B";
-//                else if(cgpa>50)
-//                    grade="P";
-//                else
-//                    grade="F";
-//
-//                Intent i=new Intent(getApplicationContext(),DisplayOutput.class);
-//                i.putExtra("NAME",Name);
-//                i.putExtra("REG_NO",regNo);
-//                i.putExtra("DEPT_NAME",deptName);
-//                i.putExtra("GENDER",gend);
-//                i.putExtra("STATE",state);
-//                i.putExtra("SUB_1",sub1.getText().toString());
-//                i.putExtra("SUB_2",sub2.getText().toString());
-//                i.putExtra("SUB_3",sub3.getText().toString());
-//                i.putExtra("SUB_4",sub4.getText().toString());
-//                i.putExtra("SUB_5",sub5.getText().toString());
-//                i.putExtra("MARK_1",mark1.getText().toString());
-//                i.putExtra("MARK_2",mark2.getText().toString());
-//                i.putExtra("MARK_3",mark3.getText().toString());
-//                i.putExtra("MARK_4",mark4.getText().toString());
-//                i.putExtra("MARK_5",mark5.getText().toString());
-//                i.putExtra("CGPA",cgpa);
-//                i.putExtra("GRADE",grade);
-//                i.putExtra("F_CHECK",fCheck);
-//                i.putExtra("B_CHECK",bCheck);
-//                i.putExtra("V_CHECK",vCheck);
-//                i.putExtra("G_CHECK",gCheck);
-//
-//                startActivity(i);
-//            }
-//        });
-//
-
-
+                        } catch (Exception e) {
+                            Log.e("TAG", "onClick: exception: " + e.getMessage());
+                        }
+                    }
+                });
     }
 }
